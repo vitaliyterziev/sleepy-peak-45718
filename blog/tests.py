cb8e287b9620caf06f7ab106e7f06bbb11f829ce
Vitaliy Terziev <vitaliyterziev@gmail.com>
2021-11-03 17:08:01 +0200

    adding tests

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+
+
+class EntryDetailViewTests(TestCase):
+
+    def enter_invalid_comment(self):
+        """
+        status 200 form is re-rendered because of value error
+        """
+        response = self.client.post(
+            reverse('blog:comment', args=(1,)), {'comment': 'shrt'})
+        self.assertEqual(response.status_code, 200)
+
+    def enter_valid_comment(self):
+        """
+        status 302 form is submitted and value is saved to DB
+        """
+        response = self.client.post(
+            reverse('blog:comment', args=(1,)), {'comment': 'long enouugh'})
+        self.assertEqual(response.status_code, 302)
